b24ff8d287f1b10f470a2d6a3c87b5620025f264
Aleksander Morawski <olek.morawski@gmail.com>
2023-10-12 13:40:22 +0200

    rewritten useffect

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,41 +1,62 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 const MapComponent = () => {
   const mapRef = useRef(null);
-  const position = [51.505, -0.09];
+  const [currentPosition, setCurrentPosition] = useState(null);
 
-  useEffect(() => {
+  const initializeMap = () => {
     if (!mapRef.current) {
-      mapRef.current = L.map("map").setView(position, 13);
+      mapRef.current = L.map("map").setView([51.505, -0.09], 13); // Fallback position
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
         mapRef.current
       );
     }
+  };
 
-    const setMapCenter = (position) => {
-      const { latitude, longitude } = position.coords;
-      if (mapRef.current) {
-        mapRef.current.setView([latitude, longitude], 13);
-      }
-    };
+  const setMapCenter = (position) => {
+    const { latitude, longitude } = position.coords;
+    const latlng = [latitude, longitude];
+    if (mapRef.current) {
+      mapRef.current.setView(latlng, 13);
+    }
+    setCurrentPosition(latlng);
+  };
 
+  const fetchGeolocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(setMapCenter, () => {
         console.log("Could not get user location");
       });
     }
+  };
 
-    return () => {
-      if (mapRef.current) {
-        mapRef.current.remove();
-        mapRef.current = null;
-      }
-    };
+  useEffect(() => {
+    initializeMap();
+    fetchGeolocation();
   }, []);
 
-  return <div id="map" style={{ height: "100vh", width: "100%" }} />;
+  const customPopupStyles = {
+    backgroundColor: "#f9f9f9",
+    border: "1px solid #ccc",
+    padding: "10px",
+    boxShadow: "0 1px 7px rgba(0, 0, 0, 0.2)",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  };
+
+  return (
+    <div id="map" style={{ height: "100vh", width: "100%" }}>
+      {currentPosition && (
+        <div style={customPopupStyles}>
+          You are at {currentPosition[0]}, {currentPosition[1]}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default MapComponent;
