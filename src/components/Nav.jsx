431f45d26510eeb5df5a74c6952701dcc85427ef
Aleksander Morawski <olek.morawski@gmail.com>
2023-10-12 14:22:20 +0200

    Nav added

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/mapview">Map View</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Nav from "../components/Nav";
 
 function Home() {
   return (
     <div>
+      <Nav />
       <h1>Welcome to the Home Page</h1>
       <p>This is a boilerplate home page.</p>
     </div>
