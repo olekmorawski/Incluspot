f1be6bf215e37596b3fac368b053152b34bf72e7
Aleksander Morawski <olek.morawski@gmail.com>
2023-10-12 13:18:33 +0200

    added normalize and fixed map and geolocation

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,28 +1,41 @@
-import React, { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import React, { useEffect, useRef } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 const MapComponent = () => {
-  const [position, setPosition] = useState([51.505, -0.09]);
+  const mapRef = useRef(null);
+  const position = [51.505, -0.09];
 
   useEffect(() => {
+    if (!mapRef.current) {
+      mapRef.current = L.map("map").setView(position, 13);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
+        mapRef.current
+      );
+    }
+
+    const setMapCenter = (position) => {
+      const { latitude, longitude } = position.coords;
+      if (mapRef.current) {
+        mapRef.current.setView([latitude, longitude], 13);
+      }
+    };
+
     if (navigator.geolocation) {
-      navigator.geolocation.watchPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        setPosition([latitude, longitude]);
+      navigator.geolocation.getCurrentPosition(setMapCenter, () => {
+        console.log("Could not get user location");
       });
     }
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.remove();
+        mapRef.current = null;
+      }
+    };
   }, []);
 
-  return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      style={{ height: "100vh", width: "100%" }}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={position} />
-    </MapContainer>
-  );
+  return <div id="map" style={{ height: "100vh", width: "100%" }} />;
 };
 
 export default MapComponent;
