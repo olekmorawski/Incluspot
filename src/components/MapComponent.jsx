082181f5bb5a8136ec77acdf3f365551a5333eaf
Aleksander Morawski <olek.morawski@gmail.com>
2023-11-27 14:41:39 +0100

    Places Popup Added, Tooltip fix

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -4,9 +4,9 @@ import "leaflet/dist/leaflet.css";
 import "leaflet.locatecontrol/dist/L.Control.Locate.min.css";
 import Locate from "leaflet.locatecontrol";
 
-const initializeMap = (mapRef) => {
+const initializeMap = (mapRef, setHasInitialViewBeenSet) => {
   if (!mapRef.current) {
-    mapRef.current = L.map("map");
+    mapRef.current = L.map("map").setView([51.505, -0.09], 13);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
       mapRef.current
     );
@@ -14,103 +14,79 @@ const initializeMap = (mapRef) => {
     const locateControl = new Locate({ position: "topright" });
     locateControl.addTo(mapRef.current);
     locateControl.start();
-  }
-};
 
-const fetchGeolocation = (
-  mapRef,
-  hasInitialViewBeenSet,
-  setHasInitialViewBeenSet
-) => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        if (mapRef.current && !hasInitialViewBeenSet) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
           mapRef.current.setView([latitude, longitude], 13);
           setHasInitialViewBeenSet(true);
+        },
+        () => {
+          console.log("Could not get user location");
         }
-      },
-      () => {
-        console.log("Could not get user location");
-      }
-    );
+      );
+    }
   }
 };
 
 const MapComponent = () => {
   const mapRef = useRef(null);
-  const [hasInitialViewBeenSet, setHasInitialViewBeenSet] = useState(false);
   const [customPopupPositions, setCustomPopupPositions] = useState([]);
+  const [hasInitialViewBeenSet, setHasInitialViewBeenSet] = useState(false);
+  const [clickListenerActive, setClickListenerActive] = useState(true); // New state variable
   let currentTooltip = null;
   let tooltipCounter = 0;
 
   const onMapClick = (event) => {
+    if (!clickListenerActive || currentTooltip) {
+      return; // Prevent new tooltip if one is already active or listener is inactive
+    }
     const latlng = event.latlng;
     const tooltipId = `tooltip-${tooltipCounter}`;
-    const buttonId = `place-popup-btn-${tooltipCounter}`;
+    const buttonId = `place-marker-btn-${tooltipCounter}`;
     tooltipCounter++;
 
     if (currentTooltip) {
-      currentTooltip.removeFrom(mapRef.current);
+      currentTooltip.remove();
     }
 
     const tooltip = L.tooltip({
       permanent: true,
       interactive: true,
-      opacity: 1,
       className: "my-custom-tooltip",
       direction: "left",
     })
       .setLatLng(latlng)
       .setContent(
-        `<div id="${tooltipId}"><button id="${buttonId}" class="custom-button">Place Popup</button></div>`
+        `<div id="${tooltipId}"><button id="${buttonId}" class="custom-button">Place Marker</button></div>`
       )
       .addTo(mapRef.current);
 
     currentTooltip = tooltip;
 
     const tooltipTimer = setTimeout(() => {
-      tooltip.removeFrom(mapRef.current);
+      tooltip.remove();
       currentTooltip = null;
-    }, 2000);
-
-    const placePopupAndRemoveTooltip = () => {
-      console.log("Attempting to place popup at:", latlng);
-
-      const newPopup = L.popup()
-        .setLatLng(latlng)
-        .setContent("There is a spot here!")
-        .openOn(mapRef.current);
-
-      console.log("Popup placed:", newPopup);
-      console.log("Popup HTML element:", newPopup.getElement());
-
-      setCustomPopupPositions([
-        ...customPopupPositions,
-        [latlng.lat, latlng.lng],
-      ]);
-
-      tooltip.removeFrom(mapRef.current);
-      document
-        .getElementById(buttonId)
-        .removeEventListener("click", placePopupAndRemoveTooltip);
-
-      currentTooltip = null;
-      clearTimeout(tooltipTimer);
-    };
+    }, 2000); // Adjust time as needed
 
     setTimeout(() => {
       const buttonElement = document.getElementById(buttonId);
       if (buttonElement) {
-        buttonElement.addEventListener("click", placePopupAndRemoveTooltip);
+        buttonElement.addEventListener("click", () => {
+          L.marker(latlng).addTo(mapRef.current);
+          setCustomPopupPositions([...customPopupPositions, latlng]);
+          tooltip.remove();
+          currentTooltip = null;
+          clearTimeout(tooltipTimer);
+          setClickListenerActive(false); // Remove the click event listener
+        });
       }
     }, 0);
   };
 
   useEffect(() => {
-    initializeMap(mapRef);
-    fetchGeolocation(mapRef, hasInitialViewBeenSet, setHasInitialViewBeenSet);
+    initializeMap(mapRef, setHasInitialViewBeenSet);
 
     if (mapRef.current) {
       mapRef.current.on("click", onMapClick);
@@ -121,7 +97,23 @@ const MapComponent = () => {
         mapRef.current.off("click", onMapClick);
       }
     };
-  }, [customPopupPositions]);
+  }, [customPopupPositions, clickListenerActive]);
+
+  useEffect(() => {
+    const enableClickListener = () => {
+      setClickListenerActive(true);
+    };
+
+    if (mapRef.current) {
+      mapRef.current.on("click", enableClickListener);
+    }
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.off("click", enableClickListener);
+      }
+    };
+  }, []);
 
   return <div id="map" style={{ height: "100vh", width: "100%" }} />;
 };
