b735971e97509f97df9e1c8d7dfe137574d1af31
Aleksander Morawski <olek.morawski@gmail.com>
2023-12-07 00:39:13 +0100

    Panel implementation

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import { useState, React } from "react";
 import MapComponent from "../components/MapComponent";
 import Panel from "../components/Panel";
 
 function Map() {
+  const [isPaneOpen, setIsPaneOpen] = useState(false);
+  const [selectedSpot, setSelectedSpot] = useState(null);
+
+  const handleSpotSelect = (spot) => {
+    setSelectedSpot(spot);
+    setIsPaneOpen(true);
+  };
+
   return (
     <div>
-      <MapComponent />
-      <Panel />
+      <MapComponent onSpotSelect={handleSpotSelect} />
+      <Panel
+        isOpen={isPaneOpen}
+        onClose={() => setIsPaneOpen(false)}
+        spot={selectedSpot}
+      />
     </div>
   );
 }
